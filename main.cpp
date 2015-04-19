64f96bc55c0a7e202bd2d0b4dc368d34668eb65d
Cuchulainn <robache.alexis@gmail.com>
2015-04-19 14:57:39 +0200

    Implémentation d'un alpha-beta minimum
    Fonction d'évaluation inchangée

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@
 int main()
 {
    Game *game = new Game();
+   cout << game->runMinimaxAlphaBeta(NULL, 10, INT_MIN, INT_MAX, true) << endl;
    //game->runMinimax();
+   /*
    for(int i = 0 ; i < 15 ; i++){
     game->generateGames(1000, i, 10);
    }
+   */
    
    return 0;
 }
